596c976d39adad0b8d6cb707c4263100f1f8ae6d
mark <markpackham1@gmail.com>
2022-06-07 14:34:23 +0100

    change flash message position

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,7 +1,6 @@
 @if (session()->has('message'))
-    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
-        class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
-        <p>
+    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" class="sticky-top">
+        <p class="text-danger">
             {{ session('message') }}
         </p>
     </div>
