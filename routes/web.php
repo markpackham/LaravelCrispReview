237fdc706d4f28cfb5c65160dbfd9006a6fb10ae
mark <markpackham1@gmail.com>
2022-06-10 15:15:13 +0100

    setup authentication

--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,15 @@ Route::get('/', [AboutController::class, 'index'])->name('home');
 // Brands
 Route::get('/brands', [BrandController::class, 'index'])->name('brands');
 
-Route::get('/brands/create', [BrandController::class, 'create'])->name('brands-create');
+Route::get('/brands/create', [BrandController::class, 'create'])->name('brands-create')->middleware('auth');
 
-Route::post('/brands', [BrandController::class, 'store']);
+Route::post('/brands', [BrandController::class, 'store'])->middleware('auth');
 
-Route::get('/brands/{brand}/edit', [BrandController::class, 'edit'])->name('brands-edit');
+Route::get('/brands/{brand}/edit', [BrandController::class, 'edit'])->name('brands-edit')->middleware('auth');
 
-Route::put('/brands/{brand}', [BrandController::class, 'update']);
+Route::put('/brands/{brand}', [BrandController::class, 'update'])->middleware('auth');
 
-Route::delete('/brands/{brand}', [BrandController::class, 'destroy']);
+Route::delete('/brands/{brand}', [BrandController::class, 'destroy'])->middleware('auth');
 
 Route::get('/brands/{brand}', [BrandController::class, 'show'])->name('brands-show');
 
@@ -42,15 +42,15 @@ Route::get('/brands/{brand}', [BrandController::class, 'show'])->name('brands-sh
 // Companies
 Route::get('/companies', [CompanyController::class, 'index'])->name('companies');
 
-Route::get('/companies/create', [CompanyController::class, 'create'])->name('companies-create');
+Route::get('/companies/create', [CompanyController::class, 'create'])->name('companies-create')->middleware('auth');
 
-Route::post('/companies', [CompanyController::class, 'store']);
+Route::post('/companies', [CompanyController::class, 'store'])->middleware('auth');
 
-Route::get('/companies/{company}/edit', [CompanyController::class, 'edit'])->name('companies-edit');
+Route::get('/companies/{company}/edit', [CompanyController::class, 'edit'])->name('companies-edit')->middleware('auth');
 
-Route::put('/companies/{company}', [CompanyController::class, 'update']);
+Route::put('/companies/{company}', [CompanyController::class, 'update'])->middleware('auth');
 
-Route::delete('/companies/{company}', [CompanyController::class, 'destroy']);
+Route::delete('/companies/{company}', [CompanyController::class, 'destroy'])->middleware('auth');
 
 Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('companies-show');
 
@@ -58,15 +58,15 @@ Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('co
 // Crisps
 Route::get('/crisps', [CrispController::class, 'index'])->name('crisps');
 
-Route::get('/crisps/create', [CrispController::class, 'create'])->name('crisps-create');
+Route::get('/crisps/create', [CrispController::class, 'create'])->name('crisps-create')->middleware('auth');
 
-Route::post('/crisps', [CrispController::class, 'store']);
+Route::post('/crisps', [CrispController::class, 'store'])->middleware('auth');
 
 Route::get('/crisps/{crisp}/edit', [CrispController::class, 'edit'])->name('crisps-edit');
 
-Route::put('/crisps/{crisp}', [CrispController::class, 'update']);
+Route::put('/crisps/{crisp}', [CrispController::class, 'update'])->middleware('auth');
 
-Route::delete('/crisps/{crisp}', [CrispController::class, 'destroy']);
+Route::delete('/crisps/{crisp}', [CrispController::class, 'destroy'])->middleware('auth');
 
 Route::get('/crisps/{crisp}', [CrispController::class, 'show'])->name('crisps-show');
 
@@ -74,15 +74,15 @@ Route::get('/crisps/{crisp}', [CrispController::class, 'show'])->name('crisps-sh
 // Flavours
 Route::get('/flavours', [FlavourController::class, 'index'])->name('flavours');
 
-Route::get('/flavours/create', [FlavourController::class, 'create'])->name('flavours-create');
+Route::get('/flavours/create', [FlavourController::class, 'create'])->name('flavours-create')->middleware('auth');
 
-Route::post('/flavours', [FlavourController::class, 'store']);
+Route::post('/flavours', [FlavourController::class, 'store'])->middleware('auth');
 
-Route::get('/flavours/{flavour}/edit', [FlavourController::class, 'edit'])->name('flavours-edit');
+Route::get('/flavours/{flavour}/edit', [FlavourController::class, 'edit'])->name('flavours-edit')->middleware('auth');
 
-Route::put('/flavours/{flavour}', [FlavourController::class, 'update']);
+Route::put('/flavours/{flavour}', [FlavourController::class, 'update'])->middleware('auth');
 
-Route::delete('/flavours/{flavour}', [FlavourController::class, 'destroy']);
+Route::delete('/flavours/{flavour}', [FlavourController::class, 'destroy'])->middleware('auth');
 
 Route::get('/flavours/{flavour}', [FlavourController::class, 'show'])->name('flavours-show');
 
@@ -91,15 +91,15 @@ Route::get('/flavours/{flavour}', [FlavourController::class, 'show'])->name('fla
 
 Route::get('/potatoes', [PotatoController::class, 'index'])->name('potatoes');
 
-Route::get('/potatoes/create', [PotatoController::class, 'create'])->name('potatoes-create');
+Route::get('/potatoes/create', [PotatoController::class, 'create'])->name('potatoes-create')->middleware('auth');
 
-Route::post('/potatoes', [PotatoController::class, 'store']);
+Route::post('/potatoes', [PotatoController::class, 'store'])->middleware('auth');
 
-Route::get('/potatoes/{potato}/edit', [PotatoController::class, 'edit'])->name('potatoes-edit');
+Route::get('/potatoes/{potato}/edit', [PotatoController::class, 'edit'])->name('potatoes-edit')->middleware('auth');
 
-Route::put('/potatoes/{potato}', [PotatoController::class, 'update']);
+Route::put('/potatoes/{potato}', [PotatoController::class, 'update'])->middleware('auth');
 
-Route::delete('/potatoes/{potato}', [PotatoController::class, 'destroy']);
+Route::delete('/potatoes/{potato}', [PotatoController::class, 'destroy'])->middleware('auth');
 
 // Always put SHOW at the end
 Route::get('/potatoes/{potato}', [PotatoController::class, 'show'])->name('potatoes-show');
@@ -107,7 +107,7 @@ Route::get('/potatoes/{potato}', [PotatoController::class, 'show'])->name('potat
 // Users
 Route::get('/register', [UserController::class, 'create'])->name('users-create');
 
-Route::get('/login', [UserController::class, 'login'])->name('users-login');
+Route::get('/login', [UserController::class, 'login'])->name('users-login')->middleware('guest');
 
 Route::post('/users', [UserController::class, 'store']);
 
