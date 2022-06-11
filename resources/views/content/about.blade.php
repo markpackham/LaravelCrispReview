f211e4227f3774f4858088e4cab44f40dcd0d320
mark <markpackham1@gmail.com>
2022-06-11 11:11:51 +0100

    add homepage image

--- resources/views/content/about.blade.php
+++ resources/views/content/about.blade.php
@@ -1,8 +1,13 @@
 <x-layout>
     <h1>Welcome to Crisp Review</h1>
     <h4>Lorem ipsum dolor sit amet.</h4>
+    <img class="img_thumbnail crisp_image_mix" src="{{ asset('images/crisp_image_mix.jpg') }}"
+        alt="Homepage collection of crisps">
     <h2>About Crisp Review</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, architecto quos nulla veritatis, sequi
-        debitis reprehenderit illum quaerat repellendus veniam, ea aliquid maiores accusamus exercitationem sit iure
-        blanditiis ex dolorem.</p>
+    <p>Just an opinionated CRUD based site on crisps. Information reguarding Potatoes and Flavours are created. Info
+        reqguarding Companies are added, then Brands associated with Companies a made.
+        Then a review of a specific Crisp is given. Each Crisp must have a Flavour and Brand added to it, the only
+        fields not required are images of the Crisp, the review description and the review score since these can be
+        added later.
+    </p>
 </x-layout>
