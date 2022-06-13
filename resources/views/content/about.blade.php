3ac40aaccaf60e1a2cad7962e811a4e0dab5777c
mark <markpackham1@gmail.com>
2022-06-13 10:32:45 +0100

    style changes

--- resources/views/content/about.blade.php
+++ resources/views/content/about.blade.php
@@ -3,7 +3,7 @@
     <h4>Lorem ipsum dolor sit amet.</h4>
     <img class="img_thumbnail crisp_image_mix" src="{{ asset('images/crisp_image_mix.jpg') }}"
         alt="Homepage collection of crisps">
-    <h2>About Crisp Review</h2>
+    <h2 class="mt-3">About Crisp Review</h2>
     <p>Just an opinionated CRUD based site on crisps. Information reguarding Potatoes and Flavours are created. Info
         reqguarding Companies are added, then Brands associated with Companies a made.
         Then a review of a specific Crisp is given. Each Crisp must have a Flavour and Brand added to it, the only
