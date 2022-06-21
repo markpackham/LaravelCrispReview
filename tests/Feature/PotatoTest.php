a8dd436e71214ab53964a1c1c3afbe8cfff7a7a2
mark <markpackham1@gmail.com>
2022-06-21 13:40:57 +0100

    test potato edit redirects

--- tests/Feature/PotatoTest.php
+++ tests/Feature/PotatoTest.php
@@ -23,6 +23,21 @@ class PotatoTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_potatoe_edit()
+    {
+        $user = User::where('id', '=', 1)->first();
+        $response = $this->actingAs($user)->get('/potatoes/1/edit');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_potatoe_edit_redirect()
+    {
+        $response = $this->get('/potatoes/1/edit');
+
+        $response->assertStatus(302);
+    }
+
     public function test_potatoes_create_guest_redirect()
     {
         $response = $this->get('/potatoes/create');
