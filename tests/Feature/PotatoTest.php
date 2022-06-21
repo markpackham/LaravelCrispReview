1abe9fab087129aa14390ffe1827ce05e300b671
mark <markpackham1@gmail.com>
2022-06-21 13:15:17 +0100

    test Potato user redirects

--- tests/Feature/PotatoTest.php
+++ tests/Feature/PotatoTest.php
@@ -2,15 +2,31 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PotatoTest extends TestCase
 {
     public function test_potatoes()
     {
         $response = $this->get('/potatoes');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_potatoes_create_guest_redirect()
+    {
+        $response = $this->get('/potatoes/create');
+
+        $response->assertStatus(302);
+    }
+
+    public function test_potatoes_create_user()
+    {
+        $user = User::where('id', '=', 1)->first();
+        $response = $this->actingAs($user)->get('/potatoes/create');
 
         $response->assertStatus(200);
     }
