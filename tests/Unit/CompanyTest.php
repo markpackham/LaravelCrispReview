8ab4934c68d6491bcb8702f9df30d304c90e1aea
mark <markpackham1@gmail.com>
2022-06-23 12:10:42 +0100

    add company creation test

--- tests/Unit/CompanyTest.php
+++ tests/Unit/CompanyTest.php
@@ -3,7 +3,8 @@
 namespace Tests\Unit;
 
 use App\Models\Company;
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Tests\TestCase;
 
 class CompanyTest extends TestCase
 {
@@ -19,5 +20,18 @@ class CompanyTest extends TestCase
         $this->assertTrue($company1->company_address === 'The Company Address');
         $this->assertTrue($company1->company_phone === '12345678');
         $this->assertTrue($company1->company_website === 'www.companywebsite.org');
+    }
+
+    public function test_company_created()
+    {
+        $user = User::where('id', '=', 1)->first();
+        $this->actingAs($user)->post('/companies', ['company_name' => 'Burts Snacks Ltd', 'company_address' => 'The Klamp House, Belliver Way, Roborough, Plymouth, PL6 7BP', 'company_phone' => '0800 023 7404', 'company_website' => 'https://www.burtssnacks.com/']);
+
+        $company1 = Company::where('company_name', '=', 'Burts Snacks Ltd')->first();
+
+        $this->assertEquals('Burts Snacks Ltd', $company1->company_name);
+        $this->assertEquals('The Klamp House, Belliver Way, Roborough, Plymouth, PL6 7BP', $company1->company_address);
+        $this->assertEquals('0800 023 7404', $company1->company_phone);
+        $this->assertEquals('https://www.burtssnacks.com/', $company1->company_website);
     }
 }
