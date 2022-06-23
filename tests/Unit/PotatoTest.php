bb8f792ed3ecb0e11a80580194cc1b50c652455b
mark <markpackham1@gmail.com>
2022-06-23 12:16:43 +0100

    test potato deleted

--- tests/Unit/PotatoTest.php
+++ tests/Unit/PotatoTest.php
@@ -29,4 +29,22 @@ class PotatoTest extends TestCase
         $this->assertEquals('Duke of York', $potato1->potato_name);
         $this->assertEquals('UK', $potato1->potato_country);
     }
+
+    public function test_potato_deleted()
+    {
+        $user = User::where('id', '=', 1)->first();
+        $this->actingAs($user)->post('/potatoes', ['potato_name' => 'Delete Spud Name', 'potato_country' => 'Delete Spun Country']);
+
+        $potato1 = Potato::where('potato_name', '=', 'Delete Spud Name')->first();
+
+        $this->assertEquals('Delete Spud Name', $potato1->potato_name);
+        $this->assertEquals('Delete Spun Country', $potato1->potato_country);
+
+        $potato1ID = $potato1->id;
+
+        $this->actingAs($user)->delete("/potatoes/$potato1ID");
+
+        $potato1 = Potato::where('potato_name', '=', 'Delete Spud Name')->first();
+        $this->assertNull($potato1);
+    }
 }
