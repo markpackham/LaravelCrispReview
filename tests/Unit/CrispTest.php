0ae50ada44522bf3ee5be1bc5cbe3374985bbb17
mark <markpackham1@gmail.com>
2022-06-24 13:36:24 +0100

    test crisp edited and deleted

--- tests/Unit/CrispTest.php
+++ tests/Unit/CrispTest.php
@@ -58,4 +58,33 @@ class CrispTest extends TestCase
 
         $this->assertNull($crisp1);
     }
+
+    public function test_crisp_edited_deleted()
+    {
+        $user = User::where('id', '=', 1)->first();
+        $this->actingAs($user)->post('/crisps', ['crisp_name' => 'Delete Crisp Name', 'crisp_weight' => 24, 'crisp_review_score' => 2, 'crisp_review' => 'Really good', 'brand_id' => 12, 'flavour_id' => 4]);
+        $crisp1 = Crisp::where('crisp_name', '=', 'Delete Crisp Name')->first();
+
+        $this->assertEquals('Delete Crisp Name', $crisp1->crisp_name);
+        $this->assertEquals(24, $crisp1->crisp_weight);
+        $this->assertEquals(2, $crisp1->crisp_review_score);
+        $this->assertEquals('Really good', $crisp1->crisp_review);
+        $this->assertEquals(12, $crisp1->brand_id);
+        $this->assertEquals(4, $crisp1->flavour_id);
+
+        $this->actingAs($user)->put("/crisps/$crisp1->id", ['crisp_name' => 'Delete Crisp Name', 'crisp_weight' => 1, 'crisp_review_score' => 1, 'crisp_review' => 'Really bad', 'brand_id' => 1, 'flavour_id' => 1]);
+        $crisp1 = Crisp::where('crisp_name', '=', 'Delete Crisp Name')->first();
+
+        $this->assertEquals('Delete Crisp Name', $crisp1->crisp_name);
+        $this->assertEquals(1, $crisp1->crisp_weight);
+        $this->assertEquals(1, $crisp1->crisp_review_score);
+        $this->assertEquals('Really bad', $crisp1->crisp_review);
+        $this->assertEquals(1, $crisp1->brand_id);
+        $this->assertEquals(1, $crisp1->flavour_id);
+
+        $this->actingAs($user)->delete("/crisps/$crisp1->id;");
+        $crisp1 = Crisp::where('crisp_name', '=', 'Delete Crisp Name')->first();
+
+        $this->assertNull($crisp1);
+    }
 }
